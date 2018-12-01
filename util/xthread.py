1fc67ef1c384344e2ca806c84897cb14c8a21f28
justworx <justworx@protonmail.com>
2018-11-30 23:19:59 -0600

    add util.xthread for thread import; possibly redundant

---
+++ util/xthread.py
@@ -0,0 +1,11 @@
+#
+# Copyright 2018 justworx
+# This file is part of the trix project, distributed under the terms 
+# of the GNU Affero General Public License.
+#
+
+try:
+	import thread
+except:
+	import _thread as thread
+
