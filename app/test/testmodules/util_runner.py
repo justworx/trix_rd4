dd1e02576eba85c07ec59f8b49c706cb7037d41d
justworx <justworx@protonmail.com>
2018-12-03 21:43:12 -0600

    add more test files

---
+++ app/test/testmodules/util_runner.py
@@ -0,0 +1,60 @@
+
+#
+# This will handle Runner, Output, and Signal(s)
+#
+
+from trix.util.runner import *
+
+class TestRunner(Runner):
+	"""
+	Test Runner features. Set TestRunner.test to any string; it will 
+	be written using `self.writeline()` on the next call to io(), and
+	then reset to None.
+	"""
+	
+	test = None
+	
+	def io(self):
+		if self.test:
+			self.writeline(self.test)
+			self.test = None
+	
+
+
+o = Buffer(encoding='utf8')
+r = TestRunner(encoding='utf_8', output=o)
+
+assert(not r.active)
+assert(not r.running)
+assert(not r.threaded)
+
+
+r.start()
+
+
+time.sleep(0.1+r.sleep)
+assert(r.active)
+assert(r.running)
+assert(r.threaded)
+
+
+r.test = "Test 1"
+
+time.sleep(0.1+r.sleep)
+assert(o.read() == "Test 1\r\n")
+
+
+r.pause()
+
+
+r.test = "Test 2"
+time.sleep(0.1+r.sleep)
+assert(r.buffer.read() == "Test 2\r\n")
+
+
+r.shutdown()
+assert(not r.active)
+assert(not r.running)
+assert(not r.threaded)
+
+
---
+++ app/test/testmodules/util_urlinfo.py
@@ -0,0 +1,29 @@
+
+#
+# This also covers util.wrap and util.inspect, at least to some
+# degree... good enough for a first draft of this test suite.
+#
+
+
+from trix.util.urlinfo import *
+from trix.util.wrap import Wrap
+
+
+u = urlinfo("http://example.com:9999/test?foo=bar")
+
+w = Wrap(u)
+for key in u.dict:
+	assert(u.dict[key] == w(key)) 
+
+
+u = urlinfo("https://bob:sue@example.com/test?foo=bar", 
+				password='Jelly'
+			)
+
+# make sure port is auto-detected
+assert(u.port==443)
+
+# make sure kwargs override parsed values
+assert(u.password=='Jelly')
+
+
